c3f2b8555dad08983e226f32060f030afa95352a
Joel Triemstra <>
2020-01-18 12:59:54 -0500

    fail; @ignore@ is pretty flaky and only ignores word characters

--- citrus-tutorial/src/test/java/com/jtriemstra/test/citrus/HttpTestRunnerIT.java
+++ citrus-tutorial/src/test/java/com/jtriemstra/test/citrus/HttpTestRunnerIT.java
@@ -41,48 +41,10 @@ public class HttpTestRunnerIT extends TestNGCitrusTestRunner  {
             //.xpath("//xh:h1", "TODO list")
             .payload("<!doctype html>\r\n" + 
             		"<html>\r\n" + 
-            		"<head>\r\n" + 
-            		"    <title>Example Domain</title>\r\n" + 
-            		"\r\n" + 
-            		"    <meta charset=\"utf-8\" />\r\n" + 
-            		"    <meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />\r\n" + 
-            		"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\r\n" + 
-            		"    <style type=\"text/css\">\r\n" + 
-            		"    body {\r\n" + 
-            		"        background-color: #f0f0f2;\r\n" + 
-            		"        margin: 0;\r\n" + 
-            		"        padding: 0;\r\n" + 
-            		"        font-family: -apple-system, system-ui, BlinkMacSystemFont, \"Segoe UI\", \"Open Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif;\r\n" + 
-            		"        \r\n" + 
-            		"    }\r\n" + 
-            		"    div {\r\n" + 
-            		"        width: 600px;\r\n" + 
-            		"        margin: 5em auto;\r\n" + 
-            		"        padding: 2em;\r\n" + 
-            		"        background-color: #fdfdff;\r\n" + 
-            		"        border-radius: 0.5em;\r\n" + 
-            		"        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\r\n" + 
-            		"    }\r\n" + 
-            		"    a:link, a:visited {\r\n" + 
-            		"        color: #38488f;\r\n" + 
-            		"        text-decoration: none;\r\n" + 
-            		"    }\r\n" + 
-            		"    @media (max-width: 700px) {\r\n" + 
-            		"        div {\r\n" + 
-            		"            margin: 0 auto;\r\n" + 
-            		"            width: auto;\r\n" + 
-            		"        }\r\n" + 
-            		"    }\r\n" + 
-            		"    </style>    \r\n" + 
+            		"<head>@ignore@" +
             		"</head>\r\n" + 
             		"\r\n" + 
-            		"<body>\r\n" + 
-            		"<div>\r\n" + 
-            		"    <h1>Example Domain</h1>\r\n" + 
-            		"    <p>This domain is for use in illustrative examples in documents. You may use this\r\n" + 
-            		"    domain in literature without prior coordination or asking for permission.</p>\r\n" + 
-            		"    <p><a href=\"https://www.iana.org/domains/example\">More information...</a></p>\r\n" + 
-            		"</div>\r\n" + 
+            		"<body>@ignore@\r\n" +             		
             		"</body>\r\n" + 
             		"</html>\r\n" + 
             		""));
