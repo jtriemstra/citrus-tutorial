79e05788ca9c4b72d8dfcfdec7eb10fd2cff7d52
Joel Triemstra <>
2020-01-18 11:58:40 -0500

    fail; new java-driven test case with no errors but doesn't execute

---
+++ citrus-tutorial/src/test/java/com/jtriemstra/test/citrus/MyFirstTestDesigner.java
@@ -0,0 +1,17 @@
+package com.jtriemstra.test.citrus;
+
+import org.testng.annotations.Test;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+
+@Test
+public class MyFirstTestDesigner extends TestNGCitrusTestDesigner {
+    @CitrusTest(name = "MyFirstTest")
+    public void myFirstTest() {
+        description("First example showing the basic test case definition elements!");
+
+        variable("text", "Hello Test Framework");
+
+        echo("${text}");
+    }
+}
